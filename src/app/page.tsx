8fc6ab0582aaeda2b73e882ed6c15e4206a71bec
ITUchiha <22030401150@darshan.ac.in>
2025-07-18 16:33:54 +0530

    02-customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import { Button } from '@/components/ui/button';
+import { Checkbox } from '@/components/ui/checkbox';
+import { Input } from '@/components/ui/input';
+import { Progress } from '@/components/ui/progress';
+import { Textarea } from '@/components/ui/textarea';
 
 export default function Home() {
   return (
-    <div>
-      <Button variant={'destructive'}>Hello</Button>
+    <div className="flex flex-col gap-y-4 p-4">
+      <div>
+        <Button variant={'elevated'}>Hello</Button>
+      </div>
+      <div>
+        <Input placeholder="I am input" />
+      </div>
+      <div>
+        <Progress value={50} />
+      </div>
+      <div>
+        <Textarea placeholder="I am text area" />
+      </div>
+      <div>
+        <Checkbox />
+      </div>
     </div>
   );
 }
